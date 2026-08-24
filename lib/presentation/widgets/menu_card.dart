3ecdf11ff30306cd275572122f9908243f795135
HonoravelMacho <tiagorabelosels@gmail.com>
2026-08-24 08:33:08 -0300

    refactor: remove MenuCard obsoleto

--- lib/presentation/widgets/menu_card.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class MenuCard extends StatelessWidget {
-  final String title; final IconData icon; final Color color; final VoidCallback onTap;
-  MenuCard({required this.title, required this.icon, required this.color, required this.onTap});
-  @override
-  Widget build(BuildContext context) {
-    return Card(child: ListTile(leading: Icon(icon, color: color), title: Text(title), onTap: onTap));
-  }
-}
