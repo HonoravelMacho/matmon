7d2a601d38fccd9c41773c26f5595ba850ab9d7b
HonoravelMacho <tiagorabelosels@gmail.com>
2026-09-01 18:37:06 -0300

    Fix: build.gradle.kts - usa jvmToolchain(17) (AGP 9.0+ moderno)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,10 +13,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     defaultConfig {
         applicationId = "com.example.matmon"
         minSdk = flutter.minSdkVersion
@@ -34,6 +30,10 @@ android {
             )
         }
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 flutter {
